5c9aa82742512dcd1212d6487ebf370506212dc2
Swoop081 <justinbelot8@gmail.com>
2026-08-31 22:12:46 +1000

    Apply v1.1.66 Superstar ability overrides before collection build

--- js/data/collection.js
+++ js/data/collection.js
@@ -1,4 +1,5 @@
 import { superstars } from "./superstars.js?v=1.1.48";
+import "../shared/v1.1.66-featured-superstar-ability-audit.js?v=1.1.66";
 import { sets } from "./sets.js?v=1.1.48";
 import { allGameplayCards } from "./content.js?v=1.1.48";
 import { CARD_NUMBER_MANIFEST, CARD_NUMBER_BY_ID, CARD_IDS_BY_SET } from "./card-number-manifest.js?v=1.1.48";
