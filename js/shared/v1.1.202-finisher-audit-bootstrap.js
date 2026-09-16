1a053c92cc7469fbff97b54864af48e3c89e99c3
Swoop081 <justinbelot8@gmail.com>
2026-09-16 21:48:22 +1000

    v1.1.202 activate released Finisher locks before app boot

---
+++ js/shared/v1.1.202-finisher-audit-bootstrap.js
@@ -0,0 +1,4 @@
+import { allGameplayCards } from "../data/content.js?v=1.1.132";
+import { applyReleasedFinisherLocks } from "./v1.1.202-released-finisher-locks.js?v=1.1.202";
+applyReleasedFinisherLocks(allGameplayCards);
+window.__WWE_LEGACY_FINISHER_AUDIT__={version:"1.1.202",applied:true};
