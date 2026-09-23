10c4f1780c7b2c6826ec8c1154979920fbd0fb6b
Swoop081 <justinbelot8@gmail.com>
2026-09-23 23:22:20 +1000

    Register Premiere gameplay move identities

--- js/data/content.js
+++ js/data/content.js
@@ -5,6 +5,7 @@ import { FUTURE_ROADMAP_GAMEPLAY_CARDS } from "./future-roadmap-v1.1.74.js?v=1.1
 import { V1175_AUTHENTICITY_CARDS } from "./v1.1.75-recommended-deck-authenticity.js?v=1.1.132";
 import { AJ_STYLES_GAMEPLAY_CARDS } from "./aj-styles-v1.1.80.js?v=1.1.132";
 import { BECKY_LYNCH_V11200_CARDS } from "./becky-lynch-v1.1.200.js?v=1.1.200";
+import { buildPremiereGameplayCards } from "./premiere-gameplay.js?v=1.1.220";
 export const allGameplayCards = [
   {
     "id": "cody-rhodes-dropdown-uppercut",
@@ -19829,6 +19830,7 @@ allGameplayCards.push(...FUTURE_ROADMAP_GAMEPLAY_CARDS, ...V1175_AUTHENTICITY_CA
 // remains active. Obsolete pre-launch reward experiments are removed rather
 allGameplayCards.push(...AJ_STYLES_GAMEPLAY_CARDS);
 allGameplayCards.push(...BECKY_LYNCH_V11200_CARDS);
+allGameplayCards.push(...buildPremiereGameplayCards(allGameplayCards));
 // than carried into a fresh-save economy.
 const RETIRED_REWARD_SET_IDS = new Set(["season-1-final-boss","parked-chyna","season-2-whos-next"]);
 for (let i=allGameplayCards.length-1;i>=0;i--) {
