b3a766f552162d20ce5da180ac51bc498d84fdf1
Swoop081 <justinbelot8@gmail.com>
2026-09-24 20:54:01 +1000

    Stamp v1.1.298 runtime lookup syntax fix

--- js/data/matchmaking.js
+++ js/data/matchmaking.js
@@ -1,6 +1,6 @@
-import { superstars } from "./superstars.js?v=1.1.297";
-import { decks } from "./decks.js?v=1.1.297";
-import { PREMIERE_STARTER_MALES, PREMIERE_STARTER_FEMALES } from "./profile.js?v=1.1.297";
+import { superstars } from "./superstars.js?v=1.1.298";
+import { decks } from "./decks.js?v=1.1.298";
+import { PREMIERE_STARTER_MALES, PREMIERE_STARTER_FEMALES } from "./profile.js?v=1.1.298";
 
 const PREMIERE_EXHIBITION_ROSTER = new Set([...PREMIERE_STARTER_MALES, ...PREMIERE_STARTER_FEMALES, "la-knight"]);
 
