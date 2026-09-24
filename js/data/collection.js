c979942e7a2fe733208943d124150e3812e04329
Swoop081 <justinbelot8@gmail.com>
2026-09-24 23:18:49 +1000

    Bump WWE Legacy to v1.1.322

--- js/data/collection.js
+++ js/data/collection.js
@@ -1,8 +1,8 @@
 import { superstars } from "./superstars.js?v=1.1.132";
 import "../shared/v1.1.66-featured-superstar-ability-audit.js?v=1.1.132";
 import { sets } from "./sets.js?v=1.1.132";
-import { allGameplayCards } from "./content.js?v=1.1.321";
-import { momentumCards } from "./decks.js?v=1.1.321";
+import { allGameplayCards } from "./content.js?v=1.1.322";
+import { momentumCards } from "./decks.js?v=1.1.322";
 import { CARD_NUMBER_MANIFEST, CARD_NUMBER_BY_ID, CARD_IDS_BY_SET } from "./card-number-manifest.js?v=1.1.132";
 import { rewardPrintingTierForSet } from "./reward-printings.js?v=1.1.132";
 
