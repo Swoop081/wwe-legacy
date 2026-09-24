a0f50ada19684829c16f77a04361e4d8bcba5f25
Swoop081 <justinbelot8@gmail.com>
2026-09-24 21:14:02 +1000

    v1.1.300 refresh deck-builder.js relaunch dependencies

--- js/data/deck-builder.js
+++ js/data/deck-builder.js
@@ -1,7 +1,7 @@
-import { decks } from "./decks.js?v=1.1.132";
+import { decks } from "./decks.js?v=1.1.300";
 import { collectionCards } from "./collection.js?v=1.1.132";
-import { allGameplayCards } from "./content.js?v=1.1.299";
-import { superstars } from "./superstars.js?v=1.1.132";
+import { allGameplayCards } from "./content.js?v=1.1.300";
+import { superstars } from "./superstars.js?v=1.1.300";
 import { evaluateDeckHealth, deckBucket } from "./deck-health.js?v=1.1.132";
 import { isPlayerReleasedSetId } from "./release.js?v=1.1.132";
 import { applyCardTier, CARD_TIERS, DEFAULT_AUTHORED_TIER, highestOwnedTier, normalizeCardTier, tierRank } from "./variants.js?v=1.1.132";
