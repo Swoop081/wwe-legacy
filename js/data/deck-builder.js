1d7881a623483fc3b2579bc79195d4ab48691973
Swoop081 <justinbelot8@gmail.com>
2026-09-24 22:49:09 +1000

    Bump WWE Legacy to v1.1.316

--- js/data/deck-builder.js
+++ js/data/deck-builder.js
@@ -1,7 +1,7 @@
-import { decks } from "./decks.js?v=1.1.315";
-import { collectionCards } from "./collection.js?v=1.1.315";
-import { allGameplayCards } from "./content.js?v=1.1.315";
-import { superstars } from "./superstars.js?v=1.1.315";
+import { decks } from "./decks.js?v=1.1.316";
+import { collectionCards } from "./collection.js?v=1.1.316";
+import { allGameplayCards } from "./content.js?v=1.1.316";
+import { superstars } from "./superstars.js?v=1.1.316";
 import { evaluateDeckHealth, deckBucket } from "./deck-health.js?v=1.1.132";
 import { isPlayerReleasedSetId } from "./release.js?v=1.1.132";
 import { applyCardTier, CARD_TIERS, DEFAULT_AUTHORED_TIER, highestOwnedTier, normalizeCardTier, tierRank } from "./variants.js?v=1.1.132";
