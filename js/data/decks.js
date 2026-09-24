ad9eac54495c2c9b8b377c3e06d8081bd2ba094e
Swoop081 <justinbelot8@gmail.com>
2026-09-24 14:21:57 +1000

    Fix launch module cache/runtime boot

--- js/data/decks.js
+++ js/data/decks.js
@@ -1,4 +1,4 @@
-import { allGameplayCards } from "./content.js?v=1.1.132";
+import { allGameplayCards } from "./content.js?v=1.1.224";
 const byId=new Map(allGameplayCards.map(c=>[c.id,c]));
 export const deckIds={
   "iyo-sky": [
