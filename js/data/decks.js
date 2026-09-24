4d4cf0b6a3f772441ee8ce4efaf2dd7a07fd21ad
Swoop081 <justinbelot8@gmail.com>
2026-09-24 14:41:20 +1000

    Synchronize v1.1.232 parser boot fix

--- js/data/decks.js
+++ js/data/decks.js
@@ -1,4 +1,4 @@
-import { allGameplayCards } from "./content.js?v=1.1.231";
+import { allGameplayCards } from "./content.js?v=1.1.232";
 const byId=new Map(allGameplayCards.map(c=>[c.id,c]));
 export const deckIds={
   "iyo-sky": [
