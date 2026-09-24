f692cfcc04787168c11120827080c141a7d9434d
Swoop081 <justinbelot8@gmail.com>
2026-09-24 18:37:51 +1000

    Refresh Premiere starter deck dependency chain for 1.1.276

--- js/data/decks.js
+++ js/data/decks.js
@@ -1,4 +1,4 @@
-import { allGameplayCards } from "./content.js?v=1.1.241";
+import { allGameplayCards } from "./content.js?v=1.1.276";
 const byId=new Map(allGameplayCards.map(c=>[c.id,c]));
 export const deckIds={
   "iyo-sky": [
