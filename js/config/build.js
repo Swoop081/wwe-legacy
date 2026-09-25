fe772765fa5bc96331dd27a81688054fdc68d6b5
Swoop081 <justinbelot8@gmail.com>
2026-09-26 07:14:15 +1000

    Bump build to 1.1.357

--- js/config/build.js
+++ js/config/build.js
@@ -1,6 +1,6 @@
 // WWE Legacy build identity + local asset cache-busting.
 // `npm run stamp-cache` rewrites this value from package.json on each release.
-export const BUILD_VERSION = "1.1.356";
+export const BUILD_VERSION = "1.1.357";
 
 export function assetUrl(path) {
   if (!path || /^(?:data:|blob:)/i.test(path)) return path;
