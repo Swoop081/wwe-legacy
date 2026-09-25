e5cb22a6a310e20419b3bbb00f10858c0efb6d79
Swoop081 <justinbelot8@gmail.com>
2026-09-25 22:32:25 +1000

    v1.1.353 stamp stylesheet recovery

--- js/data/store.js
+++ js/data/store.js
@@ -1,7 +1,7 @@
-import { superstars } from "./superstars.js?v=1.1.352";
-import { collectionCards } from "./collection.js?v=1.1.352";
-import { grantStoreSuperstarUnlockPackage, hasSuperstar, spendUniversePoints } from "./profile.js?v=1.1.352";
-import { isPlayerReleasedSetId } from "./release.js?v=1.1.352";
+import { superstars } from "./superstars.js?v=1.1.353";
+import { collectionCards } from "./collection.js?v=1.1.353";
+import { grantStoreSuperstarUnlockPackage, hasSuperstar, spendUniversePoints } from "./profile.js?v=1.1.353";
+import { isPlayerReleasedSetId } from "./release.js?v=1.1.353";
 
 export const STORE_SET_ROTATION = ["premiere"];
 export function releasedStoreSetIds(now = new Date()) { return STORE_SET_ROTATION.filter(setId => isPlayerReleasedSetId(setId, now)); }
