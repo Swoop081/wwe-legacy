54a1269a3a150ef854e0d25c7fb3fa9ef990e7b1
Swoop081 <justinbelot8@gmail.com>
2026-09-25 17:01:15 +1000

    v1.1.350 stamp legacy data purge

--- js/data/store.js
+++ js/data/store.js
@@ -1,7 +1,7 @@
-import { superstars } from "./superstars.js?v=1.1.349";
-import { collectionCards } from "./collection.js?v=1.1.349";
-import { grantStoreSuperstarUnlockPackage, hasSuperstar, spendUniversePoints } from "./profile.js?v=1.1.349";
-import { isPlayerReleasedSetId } from "./release.js?v=1.1.349";
+import { superstars } from "./superstars.js?v=1.1.350";
+import { collectionCards } from "./collection.js?v=1.1.350";
+import { grantStoreSuperstarUnlockPackage, hasSuperstar, spendUniversePoints } from "./profile.js?v=1.1.350";
+import { isPlayerReleasedSetId } from "./release.js?v=1.1.350";
 
 export const STORE_SET_ROTATION = ["premiere"];
 export function releasedStoreSetIds(now = new Date()) { return STORE_SET_ROTATION.filter(setId => isPlayerReleasedSetId(setId, now)); }
